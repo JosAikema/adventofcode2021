6aaa18014f5bbda0d7cb59d4cb58c67c61f33217
Jos Aikema <jos@nextsense.nl>
2021-12-01 08:17:31 +0100

    Day 1 NodeJS

---
+++ day1/index.js
@@ -0,0 +1,20 @@
+fs = require('fs')
+
+const inputFile = fs.readFileSync('./day1/input.txt', 'utf-8');
+
+const input = inputFile.split("\n").map((val) => parseInt(val));
+
+let inc1 = 0, inc2 = 0
+
+for (let i = 1; i < input.length; i++) {
+    if (input[i] > input[i - 1]) {
+        inc1++;
+    }
+    if (input[i] + input[i+1] + input[i+2] > input[i - 1] + input[i] + input[i+1]) {
+        inc2++;
+    }
+}
+
+console.log('Increases part 1: ' + inc1);
+console.log('Increases part 2: ' + inc2);
+
