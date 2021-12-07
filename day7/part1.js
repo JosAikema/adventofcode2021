ba529dc1df1761639423d766c4bb27b461726bda
Jos Aikema <jos@nextsense.nl>
2021-12-07 08:10:53 +0100

    Day 7 NodeJS

---
+++ day7/part1.js
@@ -0,0 +1,31 @@
+fs = require('fs')
+
+const text = fs.readFileSync('./day7/input.txt', 'utf-8')
+let input = text.split(',').map((val) => parseInt(val));;
+
+const createArray = (width, height, initialValue = null) => {
+    let array = new Array(height);
+    for (let y = 0; y < height; y++) {
+        array[y] = new Array(width);
+        for (let x = 0; x < width; x++) {
+            let value = initialValue;
+            if (typeof initialValue === 'function') value = initialValue(x, y);
+            if (Array.isArray(initialValue)) value = [...initialValue];
+            array[y][x] = Math.abs(input[x] - y);
+        }
+    }
+    return array;
+}
+
+let max = input.reduce((max,a) => a > max ? a : max);
+
+const matrix = createArray(input.length,max+1);
+
+let least = matrix[0].reduce((a,b) => a+b);
+
+for (let i=1;i <= max;i++) {
+    let sum = matrix[i].reduce((a,b) => a+b);
+    least = Math.min(least, sum);
+}
+
+console.log('Least fuel: '+least);
